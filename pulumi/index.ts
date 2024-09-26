196d052001408b2e8fb273c1a0620b4aa206ab27
sebastianthomas94 <53168297+sebastianthomas94@users.noreply.github.com>
2024-09-26 18:43:18 +0530

    done

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -3,14 +3,14 @@ import * as aws from "@pulumi/aws";
 import * as tls from "@pulumi/tls";
 
 // Create an EC2 security group
-// const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {
-//     ingress: [
-//         { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] }, // SSH
-//         { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] }, // HTTP
-//         { protocol: "tcp", fromPort: 443, toPort: 443, cidrBlocks: ["0.0.0.0/0"] }, // HTTPS
-//         { protocol: "tcp", fromPort: 3000, toPort: 3000, cidrBlocks: ["0.0.0.0/0"] }, // HTTPS
-//     ],
-// });
+const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {
+    ingress: [
+        { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] }, // SSH
+        { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] }, // HTTP
+        { protocol: "tcp", fromPort: 443, toPort: 443, cidrBlocks: ["0.0.0.0/0"] }, // HTTPS
+        { protocol: "tcp", fromPort: 3000, toPort: 3000, cidrBlocks: ["0.0.0.0/0"] }, // HTTPS
+    ],
+});
 
 // Fetch the latest Ubuntu AMI
 
@@ -18,7 +18,7 @@ const instance = new aws.ec2.Instance("instance", {
     instanceType: "t2.micro",
     ami: "ami-0e86e20dae9224db8",
     keyName: "new-pulumi", // real key pair
-    // securityGroups: [securityGroup.name],
+    securityGroups: [securityGroup.name],
     userData: `#!/bin/bash
     apt-get update -y
     `,
